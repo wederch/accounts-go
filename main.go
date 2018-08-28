79b93e4b7069f646a933c436ac724b95e8f323b0
Christian Weder <wederchr@gmail.com>
2018-08-28 15:10:10 +0200

    Initial commit of accounts service

---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+// Account structure
+type Account struct {
+	Name string
+}
+
+func main() {
+
+	accounts := &[]Account{
+		Account{"Salary"},
+		Account{"Savings"},
+		Account{"Stocks"},
+		Account{"Commodities"},
+		Account{"FX"},
+	}
+
+	http.HandleFunc("/accounts", func(w http.ResponseWriter, r *http.Request) {
+		switch m := (*r).Method; m {
+		case http.MethodGet:
+			bytes, _ := json.Marshal(accounts)
+			fmt.Fprint(w, string(bytes))
+		default:
+			serveEmptyResponse(w, r)
+		}
+	})
+
+	http.HandleFunc("/", serveEmptyResponse)
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func serveEmptyResponse(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "{}")
+}
